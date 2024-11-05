d6e8f3b53e404a32c7d5e6c788de90bdf8da332d
CUKnot <kknot2004@gmail.com>
2024-11-05 23:58:46 +0700

    change picture

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -1,15 +1,15 @@
-import BB from '../assets/pieces/BB.png';
-import BH from '../assets/pieces/BH.png';
-import BK from '../assets/pieces/BK.png';
-import BQ from '../assets/pieces/BQ.png';
-import BR from '../assets/pieces/BR.png';
-import BP from '../assets/pieces/BP.png';
-import WB from '../assets/pieces/WB.png';
-import WH from '../assets/pieces/WH.png';
-import WK from '../assets/pieces/WK.png';
-import WQ from '../assets/pieces/WQ.png';
-import WR from '../assets/pieces/WR.png';
-import WP from '../assets/pieces/WP.png';
+import BB from '../assets/pieces/Chess_bdt60.png';
+import WB from '../assets/pieces/Chess_blt60.png';
+import BK from '../assets/pieces/Chess_kdt60.png';
+import WK from '../assets/pieces/Chess_klt60.png';
+import BH from '../assets/pieces/Chess_ndt60.png';
+import WH from '../assets/pieces/Chess_nlt60.png';
+import BP from '../assets/pieces/Chess_pdt60.png';
+import WP from '../assets/pieces/Chess_plt60.png';
+import BQ from '../assets/pieces/Chess_qdt60.png';
+import WQ from '../assets/pieces/Chess_qlt60.png';
+import BR from '../assets/pieces/Chess_rdt60.png';
+import WR from '../assets/pieces/Chess_rlt60.png';
 
 const Piece = ({ type, color }) => {
   const pieceImages = {
